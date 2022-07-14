8c4a5d835acad54e9d25dba80c5e7625b79df685
felipmiguel <fmiguel@microsoft.com>
2022-07-14 12:48:15 +0200

    triggering CI/CD

--- ftalive-webapi/Controllers/WeatherForecastController.cs
+++ ftalive-webapi/Controllers/WeatherForecastController.cs
@@ -8,7 +8,7 @@ namespace ftalive_webapi.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching", "Melting", "Super-Super-Hot"
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching", "Melting", "Super-Hot", "Super-Super-Hot"
         };
 
         private readonly ILogger<WeatherForecastController> _logger;
